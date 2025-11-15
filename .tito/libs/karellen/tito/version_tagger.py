999a91203bd53b7cddd6c7c8bca05ed7f6801c65
Arcadiy Ivanov <arcadiy@ivanov.biz>
2025-11-15 01:38:04 -0500

    Remove dist_version.sh generator from Tito

--- .tito/libs/karellen/tito/version_tagger.py
+++ .tito/libs/karellen/tito/version_tagger.py
@@ -126,6 +126,4 @@ class SmartmontoolsVersionTagger(VersionTagger):
         info_out("Tagging new version of %s: %s -> %s" % (self.project_name,
                                                           old_version, new_version))
 
-        check_call(["bash", "-c", "src/getversion.sh -s > ../dist-version.sh"], text=True, cwd="smartmontools")
-
         return new_version
